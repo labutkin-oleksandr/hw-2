619d9dacbc2861c602af861ec5e10315a036dcb1
labutkin-oleksandr <84935692+labutkin-oleksandr@users.noreply.github.com>
2021-06-10 01:30:14 +0300

    Update NameEncoderDecoder.java

--- NameEncoderDecoder.java
+++ NameEncoderDecoder.java
@@ -1,4 +1,10 @@
-public String decode(String name) {            
-  String decodename = name.replace("1", "e").replace("2", "u").replace("3", "i").replace("4", "o").replace("5", "a").replace("YESNOTFORYOU", "").replace("NOTFORYOU", "");
-  return decodename;     
+class  NameEncoderDecoder {     
+  public String encode(String name) {            
+    String encodename = "NOTFORYOU" + name.replace("e", "1").replace("u", "2").replace("i", "3").replace("o", "4").replace("a", "5") + "YESNOTFORYOU";     
+    return encodename;     
+  }     
+  public String decode(String name) {            
+    String decodename = name.replace("1", "e").replace("2", "u").replace("3", "i").replace("4", "o").replace("5", "a").replace("YESNOTFORYOU", "").replace("NOTFORYOU", "");       
+    return decodename;     
+  }
 }
